3b425a4db33f701d73cb940f837416b4ab70a268
yunusgrgz1 <141489050+yunusgrgz1@users.noreply.github.com>
2025-04-28 11:26:40 +0200

    Update sending_to_s3.py

--- plugins/sending_to_s3.py
+++ plugins/sending_to_s3.py
@@ -37,7 +37,7 @@ def upload_json_to_s3(json_data):
     s3 = boto3.client(
         's3',
         aws_access_key_id=os.getenv("AWS_ACCESS_KEY_ID"),
-        aws_secret_acces_key=os.getenv("AWS_SECRET_ACCESS_KEY"),
+        aws_secret_access_key=os.getenv("AWS_SECRET_ACCESS_KEY"),
         region_name=os.getenv("AWS_DEFAULT_REGION")
     )
 
